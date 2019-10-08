192eb6c80d4337b7eb18d078d33416fa2cf521f4
ibukikuroyanagi <ibkuroyagi@gmail.com>
2019-10-09 00:10:30 +0900

    devise関連のファイルを作成

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,4 +6,12 @@ class Team < ApplicationRecord
   has_many :team_universities
   has_many :universities, through: :team_universities 
   has_many :events
+
+  validates :name, presence: true 
+  validates :date, presence: true
+  validates :prace, presence: true 
+  validates :volume, presence: true
+  validates :content, length: { maximum: 500 },presence: true 
+  validates :free, length: {maximum: 500}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'teams/index'
+  get 'teams/show'
   root to: 'event_pages#index' 
 
   # get 'university_pages/index'
@@ -12,11 +14,19 @@ Rails.application.routes.draw do
   # get 'event_pages/edit'
   # get 'event_pages/show'
   devise_for :teams
+  # teamをログアウトする際に必要になる　デフォルでuserモデルにルートが指定されてしまうから
+  devise_scope :team do
+    get '/teams/sign_out' => 'devise/sessions#destroy'
+  end
+  # teamの一覧を作成する為に必要
+  resources :users, :only => [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :event_pages #, only: [:index, :new, :edit, :show, :create, :update,:]
+  resources :event_pages 
   resources :university_pages, only: [:new, :index, :create,:update ]
 
   # Rails.application.routes.draw do
+  get 'teams/index'
+  get 'teams/show'
   #   devise_for :users, controllers: {
   #     sessions: 'users/sessions'
   #   }
