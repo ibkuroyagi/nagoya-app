58606c7d6e5264ada0dac72366dac4809eb0e679
ibukikuroyanagi <ibkuroyagi@gmail.com>
2019-11-29 10:35:41 +0900

    OSreset

--- config/routes.rb
+++ config/routes.rb
@@ -3,30 +3,20 @@ Rails.application.routes.draw do
   # get 'teams/show'
   root 'event_pages#index' 
 
-  # get 'university_pages/index'
-  # get 'university_pages/new'
-  # get 'team_pages/index'
-  # get 'team_pages/show'
-  # get 'team_pages/new'
-  # get 'team_pages/edit'
-  # get 'event_pages/index'
-  # get 'event_pages/new'
-  # get 'event_pages/edit'
-  # get 'event_pages/show'
   devise_for :teams
   # teamをログアウトする際に必要になる　デフォルでuserモデルにルートが指定されてしまうから
   devise_scope :team do
     get '/teams/sign_out' => 'devise/sessions#destroy'
   end
   # teamの一覧を作成する為に必要
-  resources :users, :only => [:index, :show]
+  resources :teams, :only => [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :event_pages 
   resources :university_pages, only: [:new, :index, :create,:update ]
 
   # Rails.application.routes.draw do
-  get 'teams/index'
-  get 'teams/show'
+  # get 'teams/index'
+  # get 'teams/show'
   #   devise_for :users, controllers: {
   #     sessions: 'users/sessions'
   #   }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+%W[A大 B大 C大 D大 E大 F大].each { |a| University.create(name: a) }
+
 Team.create!(:name => 'ユーザー１', :email => '1a@gmail.com', :password => 111111,date: "毎週木曜日午後5時〜8時", prace:"庄内緑地公園",volume: 30,content:"楽しくテニスをしています。",rink: "http://ibuki-study.net",free:"自由に詳細を記入してください！")
 Team.create!(:name => 'ユーザー2', :email => '2a@gmail.com', :password => 222222,date: "毎週木曜日午後6時〜8時", prace:"庄内公園",volume: 20,content:"楽しくテニスしています。",rink: "http://ibuki-study.net",free:"自由に詳細を記入してください！")
 Team.create(:name => 'ユーザー3', :email => '3a@gmail.com', :password => 333333,date: "毎週木曜日午後7時〜8時", prace:"緑地公園",volume: 303,content:"テニスをしています。",rink: "http://ibuki-study.net",free:"自由に詳細を記入してください！")
@@ -14,6 +16,13 @@ Team.create(:name => 'ユーザー6', :email => '6a@gmail.com', :password => 666
 Team.create(:name => 'ユーザー7', :email => '7a@gmail.com', :password => 777777,date: "毎週j曜日午後5時〜8時", prace:"庄公園",volume: 50,content:"楽しくテニスをしています。",rink: "http://ibuki-study.net",free:"自由に詳細を記入してください！")
 Team.create(:name => 'ユーザー8', :email => '8a@gmail.com', :password => 888888,date: "毎週k曜日午後5時〜8時", prace:"緑公園",volume: 2,content:"楽しくテニスをしています。",rink: "http://ibuki-study.net",free:"自由に詳細を記入してください！")
 
+
+TeamUniversity.create!(team_id: 1, university_id:1)
+TeamUniversity.create!(team_id: 1, university_id:2)
+TeamUniversity.create(team_id: 2, university_id:1)
+TeamUniversity.create(team_id: 3, university_id:1)
+TeamUniversity.create(team_id: 2, university_id:3)
+
 Event.create(name: "a1",date: "毎週木曜日午後5時〜8時",price: 100,detail: '細かい内容は頑張る',rink: 'https://ibuki-study.net',locate:"名古屋大学" ,team_id: 1)
 Event.create(name: "a2",date: "木曜日午後5時〜8時",price: 200,detail: '細かい内容は頑張2',rink: 'https://ibuki-study.net',locate:"名古屋大学", team_id: 2)
 Event.create(name: "a3",date: "毎週木曜日午後5時",price: 130,detail: 'かい内容は頑張る3aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',rink: 'https://ibuki-study.net', team_id: 1)
@@ -22,12 +31,3 @@ Event.create(name: "a5",date: "毎週木曜日午後5時〜8時",price: 500,deta
 Event.create(name: "a6",date: "毎週木曜日午後5時〜8時",price: 600,detail: '細かい内容は頑張る6',rink: 'https://ibuki-study.net', team_id: 3)
 Event.create(name: "a7",date: "毎週木曜日午後5時〜8時",price: 0,detail: '細かい内容は頑張る7',rink: 'https://ibuki-study.net', team_id: 2)
 Event.create(name: "a8",price: 500, team_id: 3)
-
-
-%W[A大 B大 C大 D大 E大 F大].each { |a| University.create(name: a) }
-
-TeamUniversity.create!(team_id: 1, university_id:1)
-TeamUniversity.create!(team_id: 1, university_id:2)
-TeamUniversity.create(team_id: 2, university_id:1)
-TeamUniversity.create(team_id: 3, university_id:1)
-TeamUniversity.create(team_id: 2, university_id:3)
