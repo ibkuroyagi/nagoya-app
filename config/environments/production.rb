44c2b0042518998fe241cde1fc7e8a5df8caf668
ibukikuroyanagi <ibkuroyagi@gmail.com>
2019-12-03 00:33:03 +0900

    gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,14 +112,22 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'https://nagoya-app.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #     :user_name => ENV['SENDGRID_USERNAME'],
+  #     :password => ENV['SENDGRID_PASSWORD'],
+  #     :domain => "heroku.com",
+  #     :address => "smtp.sendgrid.net",
+  #     :port => 587,
+  #     :authentication => :plain,
+  #     :enable_starttls_auto => true
+  # }
   config.action_mailer.smtp_settings = {
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :domain => "heroku.com",
-      :address => "smtp.sendgrid.net",
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
-  }
-
+    :user_name => ENV['MAILER_USER_ID'],
+    :password => ENV['MAILER_PASSWORD'],
+    :domain => "gmail.com",
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
 end
